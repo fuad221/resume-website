0b466f70bc1b9bda74990967650cebe6a98d1de3
fuad221 <fuad221@outlook.com>
2021-11-24 05:07:48 +0100

    I created the files according to the scheme

---
+++ src/reference-import.js
@@ -0,0 +1,8 @@
+export { default as About } from './components/pages/About';
+export { default as Educations } from './components/pages/Educations';
+export { default as Experiences } from './components/pages/Experiences';
+export { default as Home } from './components/pages/Home';
+export { default as Profile } from './components/pages/Profile';
+export { default as Portfolios } from './components/pages/Portfolios';
+export { default as Skills } from './components/pages/Skills';
+export { default as Navbar } from './components/navbar/Navbar';
